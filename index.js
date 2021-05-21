f3a642adb16022b4d2c0ab74cdda83c8ad4d8c5d
Abdullah <abdullahshafiq377@gmail.com>
2021-05-21 23:23:40 +0500

    Dice App

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
-console.log(randomNumber2);
 
-switch (randomNumber1) {
-    case 1:
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        break;
-}
-switch (randomNumber2) {
-    case 1:
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        break;
-}
-if (randomNumber1 > randomNumber2){
+var imgSrc1 = "images/dice" + randomNumber1 + ".png"
+var imgSrc2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelector(".img1").setAttribute("src", imgSrc1);
+document.querySelector(".img2").setAttribute("src", imgSrc2);
+
+if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "<em># Player 1 Wins</em>";
-}
-else if (randomNumber1 < randomNumber2){
+} else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "<em>Player 2 Wins #</em>";
-}
-else {
+} else {
     document.querySelector("h1").innerHTML = "<em>Draw</em>";
 }
