2606680ca774ff464ad7b11409f73d2b553d7095
Abdullah <abdullahshafiq377@gmail.com>
2021-05-21 23:23:40 +0500

    Dice App

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
-console.log(randomNumber2);
+
 
 switch (randomNumber1) {
     case 1:
@@ -43,12 +42,10 @@ switch (randomNumber2) {
         document.querySelector(".img2").setAttribute("src", "images/dice6.png");
         break;
 }
-if (randomNumber1 > randomNumber2){
+if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "<em># Player 1 Wins</em>";
-}
-else if (randomNumber1 < randomNumber2){
+} else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML = "<em>Player 2 Wins #</em>";
-}
-else {
+} else {
     document.querySelector("h1").innerHTML = "<em>Draw</em>";
 }
